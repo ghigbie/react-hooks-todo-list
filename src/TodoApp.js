cf9d6928144ff1d5a697fed084a8b772cf9858c3
ghigbie <gehigbie@gmail.com>
2020-01-15 21:43:21 -0700

    Add fake todos

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
-import Grid from "@material-ui/code/Grid";
+import Grid from "@material-ui/core/Grid";
 
 const paperStyles = {
     padding: 0,
@@ -17,14 +17,22 @@ const appBarStyles = {
 }
 
 const TodoApp = () => {
+    const fakeTodos = [
+        {id: 1, task: 'walk dog', completed: false },
+        {id: 2, task: 'walk cat', completed: false },
+        {id: 3, task: 'walk fish', completed: false }
+    ]
+
+    const [todos, setTodos] = useState(fakeTodos);
+
     return (
         <Paper style={paperStyles}>
            <AppBar color='primary' position='static' style={appBarStyles}>
-                <Toolbar>
+                <ToolBar>
                     <Typography color="inherit">
                         Todos with hooks
                     </Typography>
-                </Toolbar>
+                </ToolBar>
            </AppBar>
         </Paper>
     );
