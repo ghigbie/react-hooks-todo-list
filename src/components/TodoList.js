518082ee55255741f23367a293876b57e77e0b00
ghigbie <gehigbie@gmail.com>
2020-01-17 20:29:13 -0700

    Fix layout

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Diver";
+import Divider from "@material-ui/core/Divider";
 
 const TodoList = ({todos})  => {
     return (
